61479cf4b587a5ac7b13d548030d625e6ea35aa4
vanhoa690 <vanhoa690@gmail.com>
2026-03-27 15:47:32 +0700

    crud book

--- src/pages/AddPage.tsx
+++ src/pages/AddPage.tsx
@@ -1,19 +1,73 @@
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { Button, Form, Input, Select } from "antd";
+import axios from "axios";
+import { useEffect } from "react";
+type Book = {
+  id: number;
+};
+type Props = {
+  idEdit?: number | null;
+  bookDetail?: Book | undefined;
+  onSetIdEdit: (id: number | null) => void;
+};
 
-function AddPage() {
+function AddPage({ bookDetail, idEdit, onSetIdEdit }: Props) {
+  const qc = useQueryClient();
+  // onFinish
+  const onFinish = (values: any) => {
+    mutate(values);
+  };
+  // useMutation
+  const { mutate } = useMutation({
+    mutationFn: async (data: any) => {
+      if (idEdit) {
+        return await axios.put("http://localhost:3000/books/" + idEdit, data);
+      } else {
+        return await axios.post("http://localhost:3000/books", data);
+      }
+    },
+    onSuccess: () => {
+      qc.invalidateQueries({ queryKey: ["books"] });
+      form.resetFields();
+      onSetIdEdit(null);
+    },
+  });
+
+  const [form] = Form.useForm();
+  useEffect(() => {
+    form.setFieldsValue(bookDetail);
+    if (bookDetail) {
+      // reset form
+    }
+  }, [bookDetail]);
   return (
     <div className="p-6">
       <h1 className="text-2xl font-semibold mb-6">Thêm mới</h1>
 
-      <Form layout="vertical" className="space-y-6">
+      <Form
+        form={form}
+        onFinish={onFinish}
+        layout="vertical"
+        className="space-y-6"
+      >
         {/* Text input */}
-        <Form.Item label="Input" name="input">
+        <Form.Item label="Title" name="title">
           <Input placeholder="Nhập thông tin" />
         </Form.Item>
 
+        <Form.Item label="Cover Image" name="coverImage">
+          <Input placeholder="Nhập thông tin" />
+        </Form.Item>
         {/* Select */}
         <Form.Item label="Danh mục" name="category">
-          <Select placeholder="Chọn danh mục" options={[]} />
+          <Select
+            placeholder="Chọn danh mục"
+            options={[
+              { value: "Tiểu thuyết", label: "Tiểu thuyết" },
+              { value: "Tiểu thuyết 2", label: "Tiểu thuyết 2" },
+              { value: "Tiểu thuyết 3", label: "Tiểu thuyết 3" },
+            ]}
+          />
         </Form.Item>
 
         {/* Submit button */}
--- src/pages/ListPage.tsx
+++ src/pages/ListPage.tsx
@@ -1,12 +1,90 @@
-import { Table } from "antd";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import { Button, Popconfirm, Table } from "antd";
+import axios from "axios";
+import AddPage from "./AddPage";
+import { useState } from "react";
 
 function ListPage() {
+  const [idEdit, setIdEdit] = useState<number | null>(null);
+  // get data
+  const { data } = useQuery({
+    queryKey: ["books"],
+    queryFn: async () => {
+      const { data } = await axios.get("http://localhost:3000/books");
+      return data;
+    },
+  });
+
+  const { data: bookDetail } = useQuery({
+    queryKey: ["book-detail", idEdit],
+    queryFn: async () => {
+      const { data } = await axios.get("http://localhost:3000/books/" + idEdit);
+      return data;
+    },
+    enabled: !!idEdit, //co idEdit => true => chay call API
+  });
+
+  // columns
+  const columns = [
+    {
+      title: "id",
+      dataIndex: "id",
+    },
+    {
+      title: "title",
+      dataIndex: "title",
+    },
+    {
+      title: "coverImage",
+      dataIndex: "coverImage",
+    },
+    {
+      title: "Actions",
+      render: (record: any) => {
+        return (
+          <>
+            <Popconfirm
+              title="Delete Book"
+              onConfirm={() => deleteBook(record.id)}
+            >
+              <Button danger>Delete</Button>
+            </Popconfirm>
+            <Button
+              type="primary"
+              onClick={() => {
+                setIdEdit(record.id);
+              }}
+            >
+              Edit
+            </Button>
+          </>
+        );
+      },
+    },
+  ];
+
+  const qc = useQueryClient();
+
+  // useMutation: deleteBook
+  const { mutate: deleteBook } = useMutation({
+    mutationFn: async (id: number) => {
+      return await axios.delete("http://localhost:3000/books/" + id);
+    },
+    onSuccess: () => {
+      qc.invalidateQueries({ queryKey: ["books"] });
+    },
+  });
   return (
     <div className="p-6">
+      <AddPage
+        bookDetail={bookDetail}
+        idEdit={idEdit}
+        onSetIdEdit={setIdEdit}
+      />
       <h1 className="text-2xl font-semibold mb-6">Danh sách</h1>
 
       <div className="overflow-x-auto">
-        <Table columns={[]} dataSource={[]} />
+        <Table columns={columns} dataSource={data} rowKey="id" />
       </div>
     </div>
   );
