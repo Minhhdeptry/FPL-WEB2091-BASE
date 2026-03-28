9660d1724970853a77a65062fb1b4ee2feb95c1f
vanhoa690 <vanhoa690@gmail.com>
2026-03-28 15:24:47 +0700

    done

--- src/pages/AddPage.tsx
+++ src/pages/AddPage.tsx
@@ -1,19 +1,71 @@
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { Button, Form, Input, Select } from "antd";
+import axios from "axios";
+import { Detail } from "./ListPage";
+import { Dispatch, SetStateAction, useEffect } from "react";
 
-function AddPage() {
+type Props = {
+  detail: Detail | null;
+  onSetDetail: Dispatch<SetStateAction<Detail | null>>;
+};
+function AddPage({ detail, onSetDetail }: Props) {
+  // onFinish
+  const onFinish = (values: any) => {
+    mutate(values);
+  };
+  const qc = useQueryClient();
+  // useMutation
+  const { mutate } = useMutation({
+    mutationFn: async (data: any) => {
+      if (detail) {
+        // edit
+        return await axios.put(
+          "http://localhost:3000/courses/" + detail.id,
+          data,
+        );
+      } else {
+        return await axios.post("http://localhost:3000/courses", data);
+      }
+    },
+    onSuccess: () => {
+      qc.invalidateQueries();
+      form.resetFields();
+      // reset state: detail
+      onSetDetail(null);
+    },
+  });
+
+  const [form] = Form.useForm();
+  useEffect(() => {
+    if (detail) {
+      form.setFieldsValue(detail);
+    }
+  }, [detail]);
   return (
     <div className="p-6">
       <h1 className="text-2xl font-semibold mb-6">Thêm mới</h1>
 
-      <Form layout="vertical" className="space-y-6">
+      <Form
+        form={form}
+        onFinish={onFinish}
+        layout="vertical"
+        className="space-y-6"
+      >
         {/* Text input */}
-        <Form.Item label="Input" name="input">
+        <Form.Item label="Title" name="title">
           <Input placeholder="Nhập thông tin" />
         </Form.Item>
 
         {/* Select */}
         <Form.Item label="Danh mục" name="category">
-          <Select placeholder="Chọn danh mục" options={[]} />
+          <Select
+            placeholder="Chọn danh mục"
+            options={[
+              { value: "Javascript", label: "Javascript" },
+              { value: "Javascript2", label: "Javascript2" },
+              { value: "Javascript3", label: "Javascript3" },
+            ]}
+          />
         </Form.Item>
 
         {/* Submit button */}
--- src/pages/ListPage.tsx
+++ src/pages/ListPage.tsx
@@ -1,12 +1,77 @@
-import { Table } from "antd";
+import { Button, Popconfirm, Table } from "antd";
+import AddPage from "./AddPage";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import axios from "axios";
+import { useState } from "react";
 
+export type Detail = {
+  id: number;
+  title: string;
+};
 function ListPage() {
+  // columns
+
+  const [detail, setDetail] = useState<Detail | null>(null);
+
+  const handleEdit = (record: any) => {
+    console.log(record);
+    // TODO ?
+    setDetail(record);
+  };
+  const columns = [
+    {
+      title: "id",
+      dataIndex: "id",
+    },
+    {
+      title: "title",
+      dataIndex: "title",
+    },
+    {
+      title: "Actions",
+      render: (record: any) => {
+        return (
+          <>
+            <Popconfirm
+              title={"Delete Coures"}
+              onConfirm={() => mutate(record.id)}
+            >
+              <Button danger>Delete</Button>
+            </Popconfirm>
+            <Button type="primary" onClick={() => handleEdit(record)}>
+              Edit
+            </Button>
+          </>
+        );
+      },
+    },
+  ];
+  // useQuery
+  const { data } = useQuery({
+    queryKey: ["courses"],
+    queryFn: async () => {
+      const { data } = await axios.get("  http://localhost:3000/courses");
+      return data;
+    },
+  });
+
+  const qc = useQueryClient();
+  // useMutation
+  const { mutate } = useMutation({
+    mutationFn: async (id: number) => {
+      return await axios.delete("http://localhost:3000/courses/" + id);
+    },
+    onSuccess: () => {
+      qc.invalidateQueries();
+    },
+  });
   return (
     <div className="p-6">
+      <AddPage detail={detail} onSetDetail={setDetail} />
       <h1 className="text-2xl font-semibold mb-6">Danh sách</h1>
 
       <div className="overflow-x-auto">
-        <Table columns={[]} dataSource={[]} />
+        <Table columns={columns} dataSource={data} />
       </div>
     </div>
   );
